230300f9d8a9511346b7baa343f5e2bf59a88e1d
Siqi SUN <siqi.sun@thoughtworks.com>
2022-03-30 18:00:46 +1100

    extract methods

--- src/main/java/com/thoughtworks/movierental/Customer.java
+++ src/main/java/com/thoughtworks/movierental/Customer.java
@@ -23,33 +23,12 @@ public class Customer {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
         String result = "Rental Record for " + getName() + "\n";
-        for (Rental each : rentals) {
-            double thisAmount = 0;
-            //determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-                    &&
-                    each.getDaysRented() > 1) frequentRenterPoints++;
+        for (Rental rental : rentals) {
+            double thisAmount = rental.calculateRentalAmount();
+            frequentRenterPoints += rental.getFrequentRenterPoints();
 
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" +
+            result += "\t" + rental.getMovie().getTitle() + "\t" +
                     thisAmount + "\n";
             totalAmount += thisAmount;
         }
--- src/main/java/com/thoughtworks/movierental/Rental.java
+++ src/main/java/com/thoughtworks/movierental/Rental.java
@@ -17,4 +17,33 @@ public class Rental {
     public Movie getMovie() {
         return movie;
     }
+
+    public double calculateRentalAmount(){
+        double amount = 0;
+
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                amount += 2;
+                if (getDaysRented() > 2)
+                    amount += (getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                amount += getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                amount += 1.5;
+                if (getDaysRented() > 3)
+                    amount += (getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return amount;
+    }
+
+    public int getFrequentRenterPoints() {
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE)
+                &&
+                getDaysRented() > 1)
+            return 2;
+        return 1;
+    }
 }
