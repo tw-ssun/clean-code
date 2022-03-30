9f21d652b4641563423351bba711fa477440d057
Siqi SUN <siqi.sun@thoughtworks.com>
2022-03-30 16:32:52 +1100

    Characteristic test

--- src/test/java/com/thoughtworks/movierental/CustomerTest.java
+++ src/test/java/com/thoughtworks/movierental/CustomerTest.java
@@ -5,11 +5,37 @@ import org.junit.Test;
 
 public class CustomerTest {
     @Test
-    public void test() {
-
-        int expected = 3;
-
-        Assert.assertEquals(expected, 1 + 2);
+    public void testStatementGenerationWORentals() {
+        Customer customer = new Customer("testname");
+        String expected = "Rental Record for testname\n" +
+                "Amount owed is 0.0\n" +
+                "You earned 0 frequent renter points";
+        String statement = customer.statement();
+        Assert.assertEquals(expected, statement);
     }
 
+    @Test
+    public void testStatementGenerationWithRentals() {
+        Customer customer = new Customer("testname");
+        customer.addRental(new Rental(
+                new Movie("boring movie", 1),
+                2
+        ));
+        customer.addRental(new Rental(
+                new Movie("boring movie 2", 2),
+                2
+        ));
+        customer.addRental(new Rental(
+                new Movie("boring movie 3", 3),
+                2
+        ));
+        String expected = "Rental Record for testname\n" +
+                "\tboring movie\t6.0\n" +
+                "\tboring movie 2\t1.5\n" +
+                "\tboring movie 3\t0.0\n" +
+                "Amount owed is 7.5\n" +
+                "You earned 4 frequent renter points";
+        String statement = customer.statement();
+        Assert.assertEquals(expected, statement);
+    }
 }
